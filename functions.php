2c092a35a5a9c659baa2b4b61d1a1c1eb60e9162
Leon Shen <leonshen@decorrespondent.nl>
2020-07-07 23:04:25 +0200

    Fixes sticky menu height.

--- functions.php
+++ functions.php
@@ -2,5 +2,6 @@
 add_action( 'wp_enqueue_scripts', 'minim_child_enqueue_styles', 100 );
 function minim_child_enqueue_styles() {
     wp_enqueue_style( 'minim-parent', get_theme_file_uri('/style.css') );
+    wp_enqueue_style( 'custom-header-styles', get_theme_file_uri('/css/header.css') );
 }
 ?>

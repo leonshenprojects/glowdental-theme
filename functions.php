3db04b2a67e5cae4888c3e77e8d294cb3ea320ca
Leon Shen <leonshen@decorrespondent.nl>
2020-07-14 00:02:55 +0200

    Styles copyright bar.

--- functions.php
+++ functions.php
@@ -4,5 +4,6 @@ function minim_child_enqueue_styles() {
     wp_enqueue_style( 'minim-parent', get_theme_file_uri('/style.css') );
     wp_enqueue_style( 'generic-styles', get_theme_file_uri('/css/generic.css'), array(), time());
     wp_enqueue_style( 'custom-header-styles', get_theme_file_uri('/css/header.css'), array(), time());
+    wp_enqueue_style( 'custom-footer-styles', get_theme_file_uri('/css/footer.css'), array(), time());
 }
 ?>

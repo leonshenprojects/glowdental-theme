3a4487fcd1f5fdf8aa01d9b7427c007069a3ac3a
Leon Shen <leonshen@decorrespondent.nl>
2020-09-30 21:32:07 +0200

    Removes test code.

--- js/contactForms.js
+++ js/contactForms.js
@@ -1,20 +1,16 @@
 function addFormAnalytics() {
-    console.log('addFormAnalytics executed');
     document.addEventListener( 'wpcf7mailsent', function( event ) {
         switch (event.detail.contactFormId) {
             case '7707' :
                 ga('send', 'event', 'Form', 'submit', 'Contact Form');
-                console.log('Contact form sent');
                 break;
 
             case '8816' :
                 ga('send', 'event', 'Form', 'submit', 'COVID-19 Screening Form');
-                console.log('COVID-19 screening form sent');
                 break;
 
             case '8753' :
                 ga('send', 'event', 'Form', 'submit', 'Medical Questionnaire Form');
-                console.log('Medical Questionnaire Form sent');
                 break;
         }
     }, false );

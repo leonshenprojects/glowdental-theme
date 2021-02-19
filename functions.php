d7ae9a64b33bfb7184a6bd40dc60cd72f2a9dad0
Leon Shen <leonshen@decorrespondent.nl>
2021-02-20 12:13:55 +1300

    Removes datepicker fix atempt.

--- functions.php
+++ functions.php
@@ -6,19 +6,12 @@ function minim_child_enqueue_styles() {
     wp_enqueue_style( 'custom-header-styles', get_theme_file_uri('/css/header.css'), array(), '1.0' );
     wp_enqueue_style( 'custom-footer-styles', get_theme_file_uri('/css/footer.css'), array(), '1.0' );
     wp_enqueue_style( 'custom-services-styles', get_theme_file_uri('/css/services.css'), array(), '1.0' );
-    wp_enqueue_style( 'custom-form-styles', get_theme_file_uri('/css/customForm.css'), array(), '1.3' );
+    wp_enqueue_style( 'custom-form-styles', get_theme_file_uri('/css/customForm.css'), array(), '1.4' );
 
     wp_enqueue_script( 'custom-contactForms-scripts', get_theme_file_uri('/js/contactForms.js'), array(), '1.1', true );
     wp_enqueue_script( 'custom-services-scripts', get_theme_file_uri('/js/services.js'), array(), '1.0', true );
     wp_enqueue_script( 'custom-staffTiles-scripts', get_theme_file_uri('/js/staffTiles.js'), array(), '1.0', true );
     wp_enqueue_script( 'custom-font-awesome', 'https://kit.fontawesome.com/c5cc09af3e.js', array(), '1.0.0', true );
     wp_enqueue_script( 'custom-booking-widget', 'https://www.corepractice.is/Scripts/widget/client.js', array(), '1.0.0', true );
-    wp_enqueue_script( 'jquery-ui-datepicker' );
-}
-
-add_filter( 'wpcf7_support_html5_fallback', '__return_true' );
-add_action('wp_head', 'replacethiswithyourthemename_wcf7_datepickerfix');
-function replacethiswithyourthemename_wcf7_datepickerfix(){
-    ?><style>#ui-datepicker-div {z-index:99!important;}</style><?php
 }
 ?>

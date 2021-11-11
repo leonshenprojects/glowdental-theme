8198dca7902836fe9b0b0c6c62468f01781d7572
Leon Shen <leon.g.shen0709@gmail.com>
2021-11-11 01:23:43 +0100

    Redirects user to thank page on form submission.

--- js/contactForms.js
+++ js/contactForms.js
@@ -3,14 +3,17 @@ function addFormAnalytics() {
         switch (event.detail.contactFormId) {
             case '7707' :
                 ga('send', 'event', 'Form', 'submit', 'Contact Form');
+                window.location = "https://www.glowdental.co.nz/thank-you";
                 break;
 
             case '8816' :
                 ga('send', 'event', 'Form', 'submit', 'COVID-19 Screening Form');
+                window.location = "https://www.glowdental.co.nz/thank-you";
                 break;
 
             case '8753' :
                 ga('send', 'event', 'Form', 'submit', 'Medical Questionnaire Form');
+                window.location = "https://www.glowdental.co.nz/thank-you";
                 break;
         }
     }, false );
